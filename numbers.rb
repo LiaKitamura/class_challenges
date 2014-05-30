8a752eb6c0030552c194c0c0b452765ae9af80a1
Lia Kitamura <kit4mura@gmail.com>
2014-05-30 14:37:09 -0600

    numbers challenge

---
+++ numbers.rb
@@ -0,0 +1,33 @@
+# Create a new file called numbers.rb to work on this exercise.
+puts "Lets do some math:"
+
+puts "25 + 30 / 6 is "
+# Calculate and display the answer.
+puts 25 + 30 / 6
+
+puts "3 + 2 < 5 - 7 is "
+# Calculate and display the answer
+puts 3 + 2 < 5 - 7
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Determine if the statement is true or false, and display it.
+puts 3 + 2 < 5 - 7
+
+puts "Is 5 greater than -2? "
+# Determine if the answer is true or false and display it.
+puts 5 > -27
+
+puts "Is 5 greater or equal to -2? "
+# Determine if the answer is true or false and display it.
+puts 5 >= -2
+
+puts "Is 5 less than or equal to -2? "
+# Determine if the answer is true or false and display it.
+puts 5 <= -2
+
+number1 = 10
+number2 = 5
+number3 = 2
+# Use at least three methods involving one or all of these numbers
+
+puts number1 * number2 - number3
