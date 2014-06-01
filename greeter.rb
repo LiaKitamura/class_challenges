2206cd9fce6a61aaa6fbe4fe680fc4f1dfe82548
Lia Kitamura <kit4mura@gmail.com>
2014-06-01 10:32:43 -0600

    greetinger done

--- greeter.rb
+++ greeter.rb
@@ -1,5 +1,17 @@
 # Create a new file called greeter.rb to work on this exercise.
 # Create a class called Greeter that is initialized with a string that is saved
 # to the @greeting attribute.
+class Greeter
+  def initialize(string)
+    @greeting = string
+  end
+
+  def display
+    puts "#{@greeting}"
+  end
+end
+
+Greeter.new('Yo').display
+
 # Create a method that displays the string you initialized with to the console.
 # Create an instance of the Greeter class and call the display method on it.
