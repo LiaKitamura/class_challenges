26b219189c7939d9ad9bd105f38eda9635666106
Lia Kitamura <kit4mura@gmail.com>
2014-05-31 21:06:56 -0600

    while is in the bag

--- while.rb
+++ while.rb
@@ -13,35 +13,47 @@ re = false
 
 while still_hungry == true
   puts '-------Slamming down some dogs!-------'
-  eaten += 1
   puts "#{eaten}"
   puts 'Would you like more? [Y] or [N]'
   ans = gets.chomp.downcase
 
+  # answered yes
   if ans == 'y'
+    eaten += 1
     puts '-Pounding more dogs-'
+
+  # anwered no
   elsif ans == 'n'
-    puts '-Done already? Weak sauce.-'
-    puts ''
-    if eaten == 1
-      puts "You had #{eaten} hotdog!"
-    else
-      puts "You had #{eaten} hotdogs!"
-    end
     still_hungry = false
-  elsif ans != 'y' || ans != 'n'
-    redo if re == true
+    puts '-Done already? Weak sauce.-'
+
+    # 1 does not plural
+    # ternary operator
+    # puts eaten == 1 ? "You had 1 hotdizzle" : "You had #{eaten} hotdogs!"
+    # another ternary being used in string interpolation
+    puts "\nYou had #{eaten} hotdog#{eaten == 1 ? '' : 's'}"
+
+    # if eaten == 1
+    #   puts "You had #{eaten} hotdog!"
+    # else
+    #   puts "You had #{eaten} hotdogs!"
+    # end
+
+    # dogs eaten
+    if eaten <= 2
+      puts 'I guess those hotdogs were gross'
+    elsif eaten <= 4
+      puts 'Man you were hungry.'
+    else
+      puts 'Disgusting. Just plain disgusting'
+    end
+
+  # answer is anything other then y or n
+  else
     puts 'Invalid selection. [Y] or [N].'
     puts 'Really its not that hard'
     puts ''
-    eaten -= 1
-
-    if eaten == 1 || eaten == 2
-      puts 'I guess those hotdogs were gross'
-    elsif eaten == 3 || eaten == 4
-      puts 'Man you were hungry.'
-    elsif eaten >= 5
-      puts 'Disgusting. Just plain disgusting'
-    end
+    redo if re == true
   end
+
 end
