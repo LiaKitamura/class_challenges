5f1b5048cf6af2b62a16ce2e9f57c5f9a3192ce8
Lia Kitamura <kit4mura@gmail.com>
2014-05-31 21:35:35 -0600

    updated numbers challenge with stuff;

--- numbers.rb
+++ numbers.rb
@@ -1,27 +1,27 @@
 # Create a new file called numbers.rb to work on this exercise.
-puts "Lets do some math:"
+puts 'Lets do some math:'
 
-puts "25 + 30 / 6 is "
+puts '25 + 30 / 6 is '
 # Calculate and display the answer.
 puts 25 + 30 / 6
 
-puts "3 + 2 < 5 - 7 is "
+puts '3 + 2 < 5 - 7 is '
 # Calculate and display the answer
 puts 3 + 2 < 5 - 7
 
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts 'Is it true that 3 + 2 < 5 - 7?'
 # Determine if the statement is true or false, and display it.
 puts 3 + 2 < 5 - 7
 
-puts "Is 5 greater than -2? "
+puts 'Is 5 greater than -2? '
 # Determine if the answer is true or false and display it.
 puts 5 > -27
 
-puts "Is 5 greater or equal to -2? "
+puts 'Is 5 greater or equal to -2? '
 # Determine if the answer is true or false and display it.
 puts 5 >= -2
 
-puts "Is 5 less than or equal to -2? "
+puts 'Is 5 less than or equal to -2? '
 # Determine if the answer is true or false and display it.
 puts 5 <= -2
 
